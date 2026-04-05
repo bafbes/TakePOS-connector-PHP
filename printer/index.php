4e25f2bfa13c0288067b121ece966d5aa10b69cf
abb <contact@ab1consult.com>
2026-04-05 23:07:02 +0200

    Protection against double click

--- printer/index.php
+++ printer/index.php
@@ -1,19 +1,39 @@
 <?php
-header('Access-Control-Allow-Origin: *', true);
-//If file is too recent, ask for a second click just to ensure that the previous call has completely terminated
-echo "time elapsed=".(time()-filectime("invoice1.txt"))."\n";
-if (time()-filectime("invoice1.txt") <2){
+header('Access-Control-Allow-Origin: *');
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
     exit;
 }
+
+//Fichier verrou
+$f_lock = "lock.txt";
+
+//On quitte si c'est verrouillé
+if (file_exists($f_lock) && $content=file_get_contents($f_lock) === "1") {
+    echo "locked by $content\n";
+    http_response_code(429);
+    exit;
+}
+
+//Verrouillage et impression
+file_put_contents($f_lock, "1");
+
+$nprinter = $_GET["printer"] ?? 1;
+$filename = "invoice".$nprinter.".txt";
 $data=$_POST["invoice"];
 $data=base64_decode(str_pad(strtr($data, '-_', '+/'), strlen($data) % 4, '=', STR_PAD_RIGHT));
-if (empty($_GET["printer"])) $nprinter=1;
-else $nprinter=$_GET["printer"];
-echo "Printer number: ".$nprinter."<br>";
-$file = fopen("invoice".$nprinter.".txt", "w") or die("Unable to open file!");
-fwrite($file, $data);
-fclose($file);
+
+file_put_contents($filename, $data);
 $printer = file_get_contents('../printer'.$nprinter.'.ini', true);
-exec('java -jar Print.jar "'.$printer.'" '.$nprinter,$output);
-print_r( $output);
-echo 'java -jar Print.jar "'.$printer.'" '.$nprinter;
+
+exec('java -jar Print.jar "'.$printer.'" '.$nprinter, $output);
+sleep(1);
+
+//Déveerrouillage
+file_put_contents($f_lock, "0");
+
+//Logs
+echo "filename=$filename\n";
+echo "data=$data\n";
+echo "printer=$printer\n";
+print_r($output);
